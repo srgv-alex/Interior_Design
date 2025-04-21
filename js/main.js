aba905f87407c5ec9a7c9fe9175cd2b84b7cf076
unknown <jarulcrip@gmail.com>
2025-04-21 14:53:24 +0300

    add section header and carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-// FancyBox
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
-});
+$(document).ready(function () {
+	$('.header-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 600,
+		margin: 5,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+	});
 
+	$('.slider-next').click(function () {
+		$('.header-slider').trigger('next.owl.carousel');
+	});
+	$('.slider-prev').click(function () {
+		$('.header-slider').trigger('prev.owl.carousel');
+	});
+});
